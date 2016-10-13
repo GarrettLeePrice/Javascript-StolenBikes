6eeeb2f43dcb020a12fbe6ad6360926e34e1094d
garrett price and josh huffman <garrett@>
2016-10-13 11:50:59 -0700

    add markers to map with users location and pawn shops

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,26 +1,160 @@
 var googleMap = require('./../js/map.js').googleMapModule;
-
-function initializeMap(){
-  var userLatLng = new google.maps.LatLng(-34.397, 150.644);
-
-
-  var myOptions = {
-    zoom : 16,
-    center : userLatLng,
-    mapTypeId : google.maps.MapTypeId.ROADMAP
-  };
-
-  var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-  return mapObject;
-}
+var mapObject;
+var infoWindows = [];
 $(document).ready(function() {
-
-  var map = initializeMap();
-  var myLatLng = {lat: -25.363, lng: 131.044};
-  var marker = new google.maps.Marker({
-     position: myLatLng,
-     map: map,
-     title: 'Hello World!'
-   });
-
+  $('#show-map').click(function() {
+    locateUser();
+  });
 });
+
+function locateUser() {
+  if (navigator.geolocation) {
+    var positionOptions = {
+      enableHighAccuracy: true,
+      timeout: 10 * 1000
+    };
+    navigator.geolocation.getCurrentPosition(geolocationSuccess,
+      geolocationError, positionOptions);
+  } else {
+    alert("Your browser doesn't support the Geolocation API");
+  }
+}
+
+function geolocationSuccess(position) {
+  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords
+    .longitude);
+  var myOptions = {
+    zoom: 13,
+    center: userLatLng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+  // Place the marker
+  var contentString = 'You Are Here';
+  var infowindow0 = new google.maps.InfoWindow({
+    content: contentString
+  });
+  infoWindows.push(infowindow0);
+  var marker = new google.maps.Marker({
+    map: mapObject,
+    position: userLatLng,
+    icon: 'http://maps.google.com/mapfiles/arrow.png'
+  });
+  marker.addListener('click', function() {
+    infoWindows.forEach(function(value){
+      value.close();
+    });
+    infowindow0.open(mapObject, this);
+  });
+  var contentString = 'H&B Jewelry & Loan';
+  var infowindow1 = new google.maps.InfoWindow({
+    content: contentString
+  });
+  infoWindows.push(infowindow1);
+  var pawnShop1 = {
+    lat: 45.519104,
+    lng: -122.674709
+  };
+  var marker = new google.maps.Marker({
+    position: pawnShop1,
+    map: mapObject,
+    title: 'H&B Jewelry & Loan',
+    icon: 'http://labs.google.com/ridefinder/images/mm_20_green.png'
+  });
+  marker.addListener('click', function() {
+    infoWindows.forEach(function(value){
+      value.close();
+    });
+    infowindow1.open(mapObject, this);
+  });
+  contentString = 'Silver Lining Jewelry & Loan';
+  var infowindow2 = new google.maps.InfoWindow({
+    content: contentString
+  });
+  infoWindows.push(infowindow2);
+  var pawnShop2 = {
+    lat: 45.526233,
+    lng: -122.643930
+  };
+  var marker = new google.maps.Marker({
+    position: pawnShop2,
+    map: mapObject,
+    title: 'Silver Lining Jewelry & Loan',
+    icon: 'http://labs.google.com/ridefinder/images/mm_20_green.png'
+  });
+  marker.addListener('click', function() {
+    infoWindows.forEach(function(value){
+      value.close();
+    });
+    infowindow2.open(mapObject, this);
+  });
+  contentString = 'Aces Quick Cash';
+  var infowindow3 = new google.maps.InfoWindow({
+    content: contentString
+  });
+  infoWindows.push(infowindow3);
+  var pawnShop3 = {
+    lat: 45.505129,
+    lng: -122.611575
+  };
+  var marker = new google.maps.Marker({
+    position: pawnShop3,
+    map: mapObject,
+    title: 'Aces Quick Cash',
+    icon: 'http://labs.google.com/ridefinder/images/mm_20_green.png'
+  });
+  marker.addListener('click', function() {
+    infoWindows.forEach(function(value){
+      value.close();
+    });
+    infowindow3.open(mapObject, this);
+  });
+
+  contentString = 'Hawkers Locker';
+  var infowindow4 = new google.maps.InfoWindow({
+    content: contentString
+  });
+  infoWindows.push(infowindow4);
+  var pawnShop4 = {
+    lat: 45.529779,
+    lng: -122.579387
+  };
+  var marker = new google.maps.Marker({
+    position: pawnShop4,
+    map: mapObject,
+    title: 'Hawkers Locker',
+    icon: 'http://labs.google.com/ridefinder/images/mm_20_green.png'
+  });
+  marker.addListener('click', function() {
+    infoWindows.forEach(function(value){
+      value.close();
+    });
+    infowindow4.open(mapObject, this);
+  });
+
+  contentString = 'USA Pawn & Jewelry Co';
+  var infowindow5 = new google.maps.InfoWindow({
+    content: contentString
+  });
+  infoWindows.push(infowindow5);
+  var pawnShop5 = {
+    lat: 45.498349,
+    lng: -122.580266
+  };
+  var marker = new google.maps.Marker({
+    position: pawnShop5,
+    map: mapObject,
+    title: 'USA Pawn & Jewelry Co',
+    icon: 'http://labs.google.com/ridefinder/images/mm_20_green.png'
+  });
+  marker.addListener('click', function() {
+    infoWindows.forEach(function(value){
+      value.close();
+    });
+    infowindow5.open(mapObject, this);
+  });
+}
+
+function geolocationError(positionError) {
+  alert(positionError);
+}
